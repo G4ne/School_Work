a77520cfaf9481b6e145dac134eb8e5b236acf49
Aaron <114394617+G4ne@users.noreply.github.com>
2023-07-26 05:27:38 -0500

    Empty School dir to be refilled with pruned files

--- School/Railroad_Cars_Assignment/Railroad_Car.cpp
+++
@@ -1,7 +0,0 @@
-#include "Railroad_Car.h"
-
-//Getter
-double Railroad_Car::get_length() const { return length; }
-
-//Setter
-void Railroad_Car::set_length(double length) { this->length = length; }
